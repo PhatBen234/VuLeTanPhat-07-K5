90cdb58edb0a4a1c585bbd8bb7b2c68dfd28c5a2
Your Name <phatben234@gmail.com>
2025-05-15 16:38:52 +0700

    them cmt bai 7

--- VuLeTanPhat-07-K5-2.4.15/assets/Day7/Script/Bai7_D7.js
+++ VuLeTanPhat-07-K5-2.4.15/assets/Day7/Script/Bai7_D7.js
@@ -5,9 +5,9 @@ cc.Class({
     logLabel: cc.Label, // kéo node Label vào đây
   },
 
-  start() {
+  start() { //dc goi khi node được tạo
     // Truyền label xuống LazyMan để in ra
-    const lazyMan = new LazyMan('jack', this.logLabel);
+    const lazyMan = new LazyMan('jack', this.logLabel);//method chaining
     lazyMan
       .eat('apple')
       .sleep(5000)
@@ -19,23 +19,23 @@ cc.Class({
 
 class LazyMan {
   constructor(name, label) {
-    this.queue = [];
+    this.queue = []; // hang doi cac ham
     this.label = label; // giữ tham chiếu label
-    this.sayName(name);
+    this.sayName(name);// hanh dong dau tien them vao queue
     this._run(); // bắt đầu thực thi hàng đợi
   }
 
   _log(message) {
-    cc.log(message);
+    cc.log(message);// in ra console
     if (this.label) {
-      this.label.string += message + '\n';
+      this.label.string += message + '\n'; // them vao label
     }
   }
 
-  sayName(name) {
-    this.queue.push(() => {
-      this._log(`My name is ${name}`);
-      return Promise.resolve();
+  sayName(name) { // them 1 ham vao queue
+    this.queue.push(() => { // 
+      this._log(`My name is ${name}`); //lgo name
+      return Promise.resolve();//tra ve promise hoan thanh
     });
   }
 
@@ -44,25 +44,25 @@ class LazyMan {
       this._log(`I am eating ${food}`);
       return Promise.resolve();
     });
-    return this;
+    return this;// de chaining lien ket .sleep().eat() lien tuc
   }
 
   sleep(ms) {
-    this.queue.push(() => {
+    this.queue.push(() => { // them vao queue, them setTimeout()
       this._log(`I am sleeping...`);
       return new Promise(resolve => {
         setTimeout(() => {
           this._log(`After ${ms / 1000} seconds`);
           resolve();
-        }, ms);
+        }, ms);// sleep xong goi resolve
       });
     });
-    return this;
+    return this;// de chaining lien ket .sleep().eat() lien tuc
   }
 
-  async _run() {
+  async _run() { // chay lan luot action
     for (const action of this.queue) {
-      await action();
+      await action(); //await de chay lan luot
     }
   }
 }
